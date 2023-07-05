6e1ec81894849d3c18329b7588f3efe2ae361e89
Cristian Fernando Dávila López <cristian.fernando.c.davila.lopez.-nd@disney.com>
2023-07-05 15:00:12 -0500

    properties

--- src/main/java/co/com/cfd/tinteando/models/dao/ActivityDaoImpl.java
+++ src/main/java/co/com/cfd/tinteando/models/dao/ActivityDaoImpl.java
@@ -1,14 +1,18 @@
 package co.com.cfd.tinteando.models.dao;
 
 import co.com.cfd.tinteando.models.entity.Activity;
+import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class ActivityDaoImpl implements IActivityDao{
+
+    private EntityManager em;
+
     @Override
     public List<Activity> findAll() {
-        return null;
+
     }
 }
